7d430a8658fa778cb01539f2278f48ac0d7332c1
Roberto Matos <rmendozam@pucp.edu.pe>
2022-11-27 17:37:20 -0500

    commit4

---
+++ examen_parcial/examen_parcial/gestion_tareas/urls.py
@@ -0,0 +1,13 @@
+# crear rutas del proyecto
+
+from . import views 
+from django.urls import path 
+
+app_name = 'gestion_tareas'
+
+# se define las rutas de la aplicación
+
+urlpatterns = [
+    path('',views.index, name = 'index'),
+    path('ingreso',views.ingreso, name = 'ingreso')
+]
